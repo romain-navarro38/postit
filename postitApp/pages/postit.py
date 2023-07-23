61a2e8457353ca04b0dcd9a7e4aceb72b5d5b2e5
romain-navarro38 <romain.navarro38@gmail.com>
2023-07-23 21:54:41 +0200

    Add download button

--- postitApp/pages/postit.py
+++ postitApp/pages/postit.py
@@ -5,15 +5,14 @@ import pandas as pd
 st.write("# Post it")
 
 @st.cache_data
-def get_tags(data):
+def get_tags(data: dict) -> list:
     
-    tags = []
+    tags = set()
     for d in data.values():
         for tag in d["tags"]:
-            if tag not in tags:
-                tags.append(tag) 
-    
-    return tags
+            tags.add(tag)
+
+    return sorted(tags)
 
 file_upload = st.file_uploader("Uploadez votre fichier json")
 
@@ -22,14 +21,24 @@ if file_upload:
     data = json.load(file_upload)
     tags = get_tags(data)
     
-    tags_checked = {tag:st.checkbox(tag, value=True) for tag in tags}
-    
+    tags_checked = {tag: st.checkbox(tag, value=True) for tag in tags}
+
+    keys = []
     for key, val in data.items():
         for tag in val["tags"]:
-            if tags_checked[tag]:
+            if tags_checked[tag] and key not in keys:
+                keys.append(key)
                 with st.expander(key):
                     val["description"]
     
     df = pd.DataFrame(data)
     st.dataframe(df.T)
+
+    json_str = json.dumps(data)
+    st.download_button(
+        label="Télécharger votre fichier mis à jour",
+        file_name="data.json",
+        mime="application/json",
+        data=json_str,
+    )
     
